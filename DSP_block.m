9622fea02361ae9f6feb45c0205fe9c3b453756d
Jangsuyoung <boy4226@naver.com>
2015-04-10 11:48:57 +0900

    DSP_BLOCK(add Nevile_intepolation_filter)

--- DSP_block.m
+++ DSP_block.m
@@ -2,16 +2,16 @@ clear all;
 close all;
 clc;
 
-cd C:\Users\Administrator\Desktop\멤버십\창의과제1\일정\1주차\MATLAB;
+cd C:\Users\Jangs\Documents\JangProject;
 
 down_N = 3;                            % 다운 샘플링 계수
-FILTER_TYPE = 1;                       % 1 == mean 2 == linear 3 == nevile
+FILTER_TYPE = 3;                       % 1 == mean 2 == linear 3 == nevile
 
 fprintf('Down_sampling_Level : %d, Filter Type : %d\n',down_N,FILTER_TYPE);
 
 %% 음성신호 읽기 (16bit -> 8bit)
 [x_16,fs,bit]=wavread('aeoshang.wav','native'); % read speaker signal
-x_16 = x_16(1:4096);
+x_16 = x_16(1:4000*100);
 % 16 bit -> 8 bit
 con_x = int32(x_16) + 32768;
 x_8 =  uint8((con_x ./256));  % 캐스팅시 데이터가 손상된다. 1손상 256 -> 255
@@ -65,23 +65,8 @@ if(FILTER_TYPE == 3)
     %     buffer2 = zeros(BUF_SIZE ,1);
     
     %     버퍼쓰지말고 인덱싱으로 하는게 나을 거 같은데?
-    Cur = 4;
-    for i = Cur: 4 :(ndown_sample)
-        for j = 1:(down_Level-1)
-            
-            
-            
-        end
-    end
-    
-    
-    % 이거는 할당하는거
-    
-    
-    
-    
     fprintf('Nevile_filter\n');
-    
+    interp_x =  linear_interpolation_filter(up_x,down_N,ndown_sample);  
 end
 
 
@@ -91,6 +76,7 @@ con_x = int32(interp_x) * 256;   % 0 - 65280
 con_x = int16(con_x - 32768);
 
 toc();
+stem(30000:31000);
 player = audioplayer(con_x,fs);
 play(player);
 % stop(player);
